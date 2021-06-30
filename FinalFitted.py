d85ff3e52b623d0d6e5a49c9cec035c31eeda35e
Akash Krishna <34080339+AkashK23@users.noreply.github.com>
2021-06-30 08:21:44 -0400

    Update FinalFitted.py

--- FinalFitted.py
+++ FinalFitted.py
@@ -1,7 +1,11 @@
 import vtk
 import math
 import numpy as np
-import curvedSrep as cs
+import sys
+import os
+# from curvedSrep import curvedSrep as cs
+sys.path.append(os.path.abspath("."))
+from curvedSrep import curvedSrep
 import matplotlib.pyplot as plt
 from numpy import random
 from scipy.spatial import distance
@@ -9,34 +13,33 @@ from scipy.spatial import distance
 plt.figure()
 ax = plt.subplot(111, projection='3d')
 
-# s-rep of ellipsoid
+# s-rep of ellipse
 reader = vtk.vtkPolyDataReader()
-reader.SetFileName('data/reSrep.vtk')
+reader.SetFileName('sreps/data/reSrep.vtk')
 reader.Update()
 bot_srep = reader.GetOutput()
 
 #s-rep of target object
 reader2 = vtk.vtkPolyDataReader()
-reader2.SetFileName('control/top_srep_twist0.vtk')
+reader2.SetFileName('sreps/control/top_srep_twist1.vtk')
 reader2.Update()
 top_srep = reader2.GetOutput()
 
-# getting all the skeletal points from the ellipsoid's s-rep
+# get all the skeletal and boundary points of the ellipsoid s-rep
 source_pts = vtk.vtkPoints()
 for i in range(bot_srep.GetNumberOfCells()):
     base_pt_id = i * 2
     bdry_pt_id = i * 2 + 1
     s_pt = bot_srep.GetPoint(base_pt_id)
     b_pt = bot_srep.GetPoint(bdry_pt_id)
+    # ax.scatter(s_pt[0], s_pt[1], s_pt[2], color='k')
     source_pts.InsertNextPoint([s_pt[0], s_pt[1], s_pt[2]])
 source_pts.Modified()
 
 # getting 2D s-rep of the ellipsoid skeleton
-# rXs, rYs, and rZs, are arrays x,y, and z coordinate values for the skeletal points
-# rSamPts are the sampled points along the spokes of the 2D skeleton
-rXs, rYs, rZs, rSamPts = cs.curvedSrep(source_pts)
+rXs, rYs, rZs, rSamPts = curvedSrep(source_pts)
 numSamp = len(rSamPts[0])
-
+print(numSamp)
 
 iXs = []
 iYs = []
@@ -46,7 +49,6 @@ srepPts = vtk.vtkPoints()
 pPts = vtk.vtkPoints()
 
 # viewing the 2D skeleton
-# plots the 2D skeleton using MatPlotLib
 ptsOnSkel = []
 for i in range(0, len(rXs)):
     intPt = (rXs[i], rYs[i], rZs[i])
@@ -80,10 +82,10 @@ for i in range(0, len(rXs)):
             srepPts.InsertNextPoint([intPt[0], intPt[1], intPt[2]])
             ptsOnSkel.append([intPt[0], intPt[1], intPt[2]])
             ax.scatter(intPt[0], intPt[1], intPt[2], color='b')
-            if j == 0:
-                ax.plot([iXs[i], iSpoke[0][0]], [iYs[i], iSpoke[0][1]], [iZs[i], iSpoke[0][2]], 'r')
-            else:
-                ax.plot([iSpoke[j-1][0], iSpoke[j][0]], [iSpoke[j-1][1], iSpoke[j][1]], [iSpoke[j-1][2], iSpoke[j][2]], 'r')
+            # if j == 0:
+            #     ax.plot([iXs[i], iSpoke[0][0]], [iYs[i], iSpoke[0][1]], [iZs[i], iSpoke[0][2]], 'r')
+            # else:
+            #     ax.plot([iSpoke[j-1][0], iSpoke[j][0]], [iSpoke[j-1][1], iSpoke[j][1]], [iSpoke[j-1][2], iSpoke[j][2]], 'r')
         iSamPts.append(iSpoke)
     else:
         iSpoke = []
@@ -120,8 +122,10 @@ srepSet.SetPoints(srepPts)
 srepSet.Modified()
 ax.plot(iXs, iYs, iZs, 'r')
 
-# getting all the skeletal and boundary points of the ellipsoid's skeleton
-# Need thin plate splines to interpolate the neighboring 3D spokes around a given spoke
+print(len(rXs))
+print(len(ptsOnSkel))
+
+# using thin plate splines to be able to interpolate 3D spokes on the ellipsoid s-rep
 source_pts2 = vtk.vtkPoints()
 target_pts2 = vtk.vtkPoints()
 for i in range(math.floor((bot_srep.GetNumberOfCells() -24) / 2)):
@@ -137,14 +141,14 @@ tps.SetTargetLandmarks(target_pts2)
 tps.SetBasisToR()
 tps.Modified()
 
-# mesh of the best fitting ellipsoid
+# ellipsoid mesh
 reader3 = vtk.vtkPolyDataReader()
-reader3.SetFileName('data/recenter.vtk')
+reader3.SetFileName('sreps/data/recenter.vtk')
 reader3.Update()
 bot_mesh = reader3.GetOutput()
 # mesh of target object
 reader4 = vtk.vtkPolyDataReader()
-reader4.SetFileName('control/FinTopMesh0.vtk')
+reader4.SetFileName('sreps/control/FinTopMesh1.vtk')
 reader4.Update()
 top_mesh = reader4.GetOutput()
 
@@ -153,13 +157,18 @@ target_pts3 = vtk.vtkPoints()
 
 ellPts = bot_mesh.GetNumberOfPoints()
 
+# getting points on ellipsoid and target object mesh
 for i in range(0, ellPts):
     pt = [0] * 3
     bot_mesh.GetPoint(i, pt)
+    # print(pt)
     source_pts3.InsertNextPoint(pt)
 
     top_mesh.GetPoint(i, pt)
+    # print(pt)
     target_pts3.InsertNextPoint(pt)
+    # if i == 5:
+    #     break
 source_pts3.Modified()
 target_pts3.Modified()
 
@@ -171,8 +180,6 @@ tps2.SetBasisToR()
 tps2.Modified()
 
 # method to find closest points on 2D skeleton
-# Given a skeletal point on the 3D s-rep, it finds the closest discrete point on the 2D s-rep
-# This will be replaced once interpolator is finished
 def findClosetPt(pt, pts):
     # test = pt[2]
     dist = (pt[0]-pts[0][0])**2+(pt[1]-pts[0][1])**2+(pt[2]-pts[0][2])**2
@@ -209,24 +216,14 @@ def findClosetPt(pt, pts):
         else:
             ind1 = nextPt
 
-    print([ind0, ind1])
+    # print([ind0, ind1])
     return dist, [ind0, ind1]
 
 fitFrames = vtk.vtkPolyData()
 fitFrames_ends = vtk.vtkPoints()
 fitFrames_lines = vtk.vtkCellArray()
 
-# loop through all the spokes on s-rep
-# Calculates the neighboring points for every sampled point on 3D s-rep
-# using TPS, determines the location of the neighboring points on the target object s-rep
-
-# get U and T from the closest points on 2d skel
-# use TPS to interpolate 3D spoke # 2 in U sides and 2 in T sides
-# sample points on the actual spokes and the interpolated spokes
-# use TPS to find where those sampled points would be in actual 3d object
-# get 3d mesh, ellipsoid points as source and target object points as target
-# tps the neighboring points for the frames for the target object
-
+# finding closest 2D s-rep points on 2D s-rep
 for i in range(0, math.floor((bot_srep.GetNumberOfCells() -24) / 2)):
     base_pt_id = i * 2
     bdry_pt_id = i * 2 + 1
@@ -239,14 +236,41 @@ for i in range(0, math.floor((bot_srep.GetNumberOfCells() -24) / 2)):
 
     numSamp = 5
     eps = 0.5
-    print("hi")
-    print(type(ptsOnSkel))
+    
     dist, ind = findClosetPt([s_pt[0], s_pt[1], s_pt[2]], ptsOnSkel)
-    # ax.scatter(s_pt[0], s_pt[1], s_pt[2], color='r')   
-    ax.scatter(ptsOnSkel[ind[0]][0], ptsOnSkel[ind[0]][1], ptsOnSkel[ind[0]][2], color='r')
-    ax.scatter(ptsOnSkel[ind[1]][0], ptsOnSkel[ind[1]][1], ptsOnSkel[ind[1]][2], color='r')
-    print(ind)
-    uDir = [ptsOnSkel[ind[1]][0] - ptsOnSkel[ind[0]][0], ptsOnSkel[ind[1]][1] - ptsOnSkel[ind[0]][1], ptsOnSkel[ind[1]][2] - ptsOnSkel[ind[0]][2]]
+    # ax.scatter(s_pt[0], s_pt[1], s_pt[2], color='r')
+
+    spk0 = math.floor(ind[0] / numSamp)
+    spk1 = math.floor(ind[1] / numSamp)
+
+    # if spk0 == len(rXs) -1:
+    #     ax.scatter(ptsOnSkel[ind[0]][0], ptsOnSkel[ind[0]][1], ptsOnSkel[ind[0]][2], color='r')
+    #     ax.scatter(ptsOnSkel[ind[1]][0], ptsOnSkel[ind[1]][1], ptsOnSkel[ind[1]][2], color='r')
+    
+
+    if spk0 % 2 == 1:
+        if spk1 < spk0:
+            uDir = [ptsOnSkel[ind[1]][0] - ptsOnSkel[ind[0]][0], ptsOnSkel[ind[1]][1] - ptsOnSkel[ind[0]][1], ptsOnSkel[ind[1]][2] - ptsOnSkel[ind[0]][2]]
+        else:
+            uDir = [ptsOnSkel[ind[0]][0] - ptsOnSkel[ind[1]][0], ptsOnSkel[ind[0]][1] - ptsOnSkel[ind[1]][1], ptsOnSkel[ind[0]][2] - ptsOnSkel[ind[1]][2]]
+    else:
+        if spk1 > spk0:
+            uDir = [ptsOnSkel[ind[1]][0] - ptsOnSkel[ind[0]][0], ptsOnSkel[ind[1]][1] - ptsOnSkel[ind[0]][1], ptsOnSkel[ind[1]][2] - ptsOnSkel[ind[0]][2]]
+        else:
+            uDir = [ptsOnSkel[ind[0]][0] - ptsOnSkel[ind[1]][0], ptsOnSkel[ind[0]][1] - ptsOnSkel[ind[1]][1], ptsOnSkel[ind[0]][2] - ptsOnSkel[ind[1]][2]]
+    if spk0 == 0:
+        if spk1 % 2 == 1:
+            uDir = [ptsOnSkel[ind[0]][0] - ptsOnSkel[ind[1]][0], ptsOnSkel[ind[0]][1] - ptsOnSkel[ind[1]][1], ptsOnSkel[ind[0]][2] - ptsOnSkel[ind[1]][2]]
+        else:
+            uDir = [ptsOnSkel[ind[1]][0] - ptsOnSkel[ind[0]][0], ptsOnSkel[ind[1]][1] - ptsOnSkel[ind[0]][1], ptsOnSkel[ind[1]][2] - ptsOnSkel[ind[0]][2]]           
+    if ind[0] >= len(ptsOnSkel) - numSamp:
+        if spk1 % 2 == 1:
+            uDir = [ptsOnSkel[ind[1]][0] - ptsOnSkel[ind[0]][0], ptsOnSkel[ind[1]][1] - ptsOnSkel[ind[0]][1], ptsOnSkel[ind[1]][2] - ptsOnSkel[ind[0]][2]]
+        else:
+            uDir = [ptsOnSkel[ind[0]][0] - ptsOnSkel[ind[1]][0], ptsOnSkel[ind[0]][1] - ptsOnSkel[ind[1]][1], ptsOnSkel[ind[0]][2] - ptsOnSkel[ind[1]][2]]
+
+
+    # uDir = [ptsOnSkel[ind[1]][0] - ptsOnSkel[ind[0]][0], ptsOnSkel[ind[1]][1] - ptsOnSkel[ind[0]][1], ptsOnSkel[ind[1]][2] - ptsOnSkel[ind[0]][2]]
     # print(ptsOnSkel[125])
     # print(ptsOnSkel[130])
     length = math.sqrt(uDir[0]**2 + uDir[1]**2 + uDir[2]**2)
@@ -327,6 +351,9 @@ for i in range(0, math.floor((bot_srep.GetNumberOfCells() -24) / 2)):
         unit = lengthUL/(spokeSam-1)
         uLpt = [uL[0] + vecUL[0]*j*unit, uL[1] + vecUL[1]*j*unit, uL[2] + vecUL[2]*j*unit]
 
+        # if spk0 == len(rXs) -1:
+        #     ax.scatter(spokePt[0], spokePt[1], spokePt[2], color='k')
+
         spokeT = tps2.TransformPoint(spokePt)
         tUtrans = tps2.TransformPoint(tUpt)
         tDtrans = tps2.TransformPoint(tDpt)
@@ -339,8 +366,9 @@ for i in range(0, math.floor((bot_srep.GetNumberOfCells() -24) / 2)):
         # ax.scatter(uLpt[0], uLpt[1], uLpt[2], color='k')
         # ax.scatter(tUtrans[0], tUtrans[1], tUtrans[2], color='k')
         # ax.scatter(tDtrans[0], tDtrans[1], tDtrans[2], color='r')
-        # ax.scatter(uRtrans[0], uRtrans[1], uRtrans[2], color='b')
-        # ax.scatter(uLtrans[0], uLtrans[1], uLtrans[2], color='b')
+        # if j == 4:
+        #     ax.scatter(uRtrans[0], uRtrans[1], uRtrans[2], color='k')
+        #     ax.scatter(uLtrans[0], uLtrans[1], uLtrans[2], color='r')
 
         # ax.scatter(tU[0]+ tU[0]*4*unit, tU[1]+ vecTU[1]*4*unit, tU[2]+ vecTU[2]*4*unit, color='k')
         # ax.scatter(ptsOnSkel[ind[0]][0], ptsOnSkel[ind[0]][1], ptsOnSkel[ind[0]][2], color='r')
@@ -391,37 +419,82 @@ for i in range(0, math.floor((bot_srep.GetNumberOfCells() -24) / 2)):
                 vec3 = np.cross(uVec, norm1)
 
         
-        finT = [spokeT[0] + vec3[0], spokeT[1] + vec3[1], spokeT[2] + vec3[2]]
+        finT1 = [spokeT[0] + vec3[0], spokeT[1] + vec3[1], spokeT[2] + vec3[2]]
+        normToPt1 = [tUtrans[0] - finT1[0], tUtrans[1] - finT1[1], tUtrans[2] - finT1[2]]
+        normDist1 = np.linalg.norm(normToPt1)
+
+        finT2 = [spokeT[0] - vec3[0], spokeT[1] - vec3[1], spokeT[2] - vec3[2]]
+        normToPt2 = [tUtrans[0] - finT2[0], tUtrans[1] - finT2[1], tUtrans[2] - finT2[2]]
+        normDist2 = np.linalg.norm(normToPt2)
+
+        boo1 = False
+        if normDist1 < normDist2:
+            finT = finT1
+        else:
+            finT = finT2
+            vec3 = np.cross(uVec, norm2)
+
+        # if j == 4:
+        #     if boo1:
+        #         finN = finN2
+        #         vec3 = np.cross(uVec, norm2)
+        #     else:
+        #         finN = finN1
+        #         vec3 = np.cross(uVec, norm1)
 
 
 
+
+
+        # if spk0 != 0 and spk0 != len(rXs) -1:
         ax.plot([spokeT[0], finU[0]], [spokeT[1], finU[1]], [spokeT[2], finU[2]], 'k')
         ax.plot([spokeT[0], finT[0]], [spokeT[1], finT[1]], [spokeT[2], finT[2]], 'k')
         ax.plot([spokeT[0], finN[0]], [spokeT[1], finN[1]], [spokeT[2], finN[2]], 'k')
 
-        id0 = fitFrames_ends.InsertNextPoint(tuple(spokeT))
+        id0 = fitFrames_ends.InsertNextPoint(tuple(spokePt))
         id1 = fitFrames_ends.InsertNextPoint(tuple(finU))
         spoke_line = vtk.vtkLine()
         spoke_line.GetPointIds().SetId(0, id0)
         spoke_line.GetPointIds().SetId(1, id1)
         fitFrames_lines.InsertNextCell(spoke_line)
 
-        id0 = fitFrames_ends.InsertNextPoint(tuple(spokeT))
+        id0 = fitFrames_ends.InsertNextPoint(tuple(spokePt))
         id1 = fitFrames_ends.InsertNextPoint(tuple(finT))
         spoke_line = vtk.vtkLine()
         spoke_line.GetPointIds().SetId(0, id0)
         spoke_line.GetPointIds().SetId(1, id1)
         fitFrames_lines.InsertNextCell(spoke_line)
 
-        id0 = fitFrames_ends.InsertNextPoint(tuple(spokeT))
+        id0 = fitFrames_ends.InsertNextPoint(tuple(spokePt))
         id1 = fitFrames_ends.InsertNextPoint(tuple(finN))
         spoke_line = vtk.vtkLine()
         spoke_line.GetPointIds().SetId(0, id0)
         spoke_line.GetPointIds().SetId(1, id1)
         fitFrames_lines.InsertNextCell(spoke_line)
 
-#     if i == 1:
-#         break
+    # ax.plot([tU[0], spokeTU[0]], [tU[1], spokeTU[1]], [tU[2], spokeTU[2]], 'b')
+    # ax.plot([tD[0], spokeTD[0]], [tD[1], spokeTD[1]], [tD[2], spokeTD[2]], 'b')
+    # ax.plot([s_pt[0], spoke[0]], [s_pt[1], spoke[1]], [s_pt[2], spoke[2]], 'r')
+    # ax.plot([uR[0], spokeUR[0]], [uR[1], spokeUR[1]], [uR[2], spokeUR[2]], 'b')
+    # ax.plot([uL[0], spokeUL[0]], [uL[1], spokeUL[1]], [uL[2], spokeUL[2]], 'b')
+
+    # ax.plot([s_pt[0], s_pt[0]+finTao[0]], [s_pt[1], s_pt[1]+finTao[1]], [s_pt[2], s_pt[2]+finTao[2]], 'k')
+    # ax.plot([s_pt[0], s_pt[0]-finTao[0]], [s_pt[1], s_pt[1]-finTao[1]], [s_pt[2], s_pt[2]-finTao[2]], 'k')
+    # ax.plot([s_pt[0], s_pt[0]+uDir[0]], [s_pt[1], s_pt[1]+uDir[1]], [s_pt[2], s_pt[2]+uDir[2]], 'k')
+    # ax.plot([s_pt[0], s_pt[0]-uDir[0]], [s_pt[1], s_pt[1]-uDir[1]], [s_pt[2], s_pt[2]-uDir[2]], 'k')
+    # print(finTao)
+    # print(uDir)
+    # print(str(i) + " index")
+    # if i == 100:
+    #     break
+
+    # get U and T from the closest points on 2d skel
+    # use TPH to interpolate 3D spoke # 2 in U sides and 2 in T sides
+    # sample points on the actual spokes and the interpolated spokes
+    # use TPH to find where those sampled points would be in actual 3d object
+    #   get 3d mesh, ellipsoid points as source and target object points as target
+    # tph the neighboring points for the frames for the target object
+
 
 plt.show()
 
