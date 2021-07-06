4a44af043074c43c3ff5ccf49efe90385b26e8a2
Akash Krishna <34080339+AkashK23@users.noreply.github.com>
2021-07-06 12:31:45 -0400

    Update fit2D.py

--- fit2D.py
+++ fit2D.py
@@ -377,7 +377,7 @@ def ell_srep(diffeoX, diffeoY, radLen, finRotAng, means):
     skelLen = math.sqrt((skeletonXs[1]-skeletonXs[0])**2 + (skeletonYs[1]-skeletonYs[0])**2) /2
 
     # number of skeletal points
-    numSpoke = 21
+    numSpoke = 20
     theta = -1*math.pi
     sPoints = []
     preTpt = []
